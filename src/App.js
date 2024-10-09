6d1ed84de0e79ed647f9dab589811377c0ba57c9
jasurgraduate <jasur@graduate.org>
2024-10-10 02:03:24 +0900

    new features :)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Quiz from './components/Quiz';
+import Quiz from './components/Quiz'; // Import your Quiz component
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/quiz" element={<Quiz />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      {/* Render the Quiz component directly */}
+      <Quiz />
+    </div>
   );
 };
 
